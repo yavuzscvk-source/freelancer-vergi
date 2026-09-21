184f79022e90793a76cdb9a6e6fa9d0e4c2f8d88
yavuzscvk-source <yavuzscvk@gmail.com>
2026-09-21 16:30:20 +0300

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,5 +6,6 @@ export default function sitemap() {
     { url: `${SITE_URL}/serbest-meslek-makbuzu`, lastModified: new Date() },
     { url: `${SITE_URL}/gelir-vergisi-hesaplama`, lastModified: new Date() },
     { url: `${SITE_URL}/genc-girisimci-istisnasi`, lastModified: new Date() },
+    { url: `${SITE_URL}/sahis-mi-limited-mi`, lastModified: new Date() },
   ];
 }
