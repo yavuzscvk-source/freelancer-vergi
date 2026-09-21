014637017ebea1494d28a1446559e8b61aa9e33f
yavuzscvk-source <yavuzscvk@gmail.com>
2026-09-21 06:10:39 +0300

    Create robots.js

---
+++ app/robots.js
@@ -0,0 +1,8 @@
+import { SITE_URL } from "../lib/site";
+
+export default function robots() {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: `${SITE_URL}/sitemap.xml`,
+  };
+}
